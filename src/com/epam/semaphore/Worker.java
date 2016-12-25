1f19b75cef4cc60d7dd817666c16b9f3a2ba5765
BohdanTfx <noreply>
2016-12-25 17:07:01 +0200

    All at once start added

--- src/com/epam/semaphore/Worker.java
+++ src/com/epam/semaphore/Worker.java
@@ -1,21 +1,30 @@
 package com.epam.semaphore;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Worker implements Runnable {
     private static AtomicInteger finishedThreads = new AtomicInteger(0);
-    private Semaphore semaphore = new Semaphore(1);
+    private static final Semaphore SEMAPHORE = new Semaphore(1);
+    private static final CyclicBarrier barrier = new CyclicBarrier(App.THREADS_COUNT);
 
     @Override
     public void run() {
         String threadName = Thread.currentThread().getName();
+        try {
+            System.out.println(threadName + " waiting");
+            barrier.await();
+        } catch (InterruptedException | BrokenBarrierException e1) {
+            e1.printStackTrace();
+        }
         for (int i = 0; i < 5; i++) {
             try {
-                semaphore.acquire();
+                SEMAPHORE.acquire();
                 System.out.println("ping " + threadName);
                 System.out.println("PPPOOONNNGGG " + threadName);
-                semaphore.release();
+                SEMAPHORE.release();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
